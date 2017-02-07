8573934ee73db14da5670ab34b3c7a2ec7bad2b5
androiddrew <bednar.andrew@gmail.com>
2017-02-07 12:58:58 -0500

    Added auth view and tests for registration

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -7,3 +7,52 @@ from project.server import bcrypt, db
 from project.server.models import User
 
 auth_blueprint = Blueprint('auth', __name__)
+
+class RegisterAPI(MethodView):
+    """User Registration Resource"""
+
+    def post(self):
+        # Get the post data
+        post_data = request.get_json()
+        # check of user already exists
+        user = User.query.filter_by(email=post_data.get('email')).first()
+        if not user:
+            try:
+                user = User(
+                    email=post_data.get('email'),
+                    password=post_data.get('password')
+                )
+
+                #insert new user
+                db.session.add(user)
+                db.session.commit()
+                # gen auth token
+                auth_token = User.encode_auth_token(user.id)
+                responseObject = {
+                    "status": "success",
+                    "message": "Successfully registered.",
+                    "auth_token": auth_token.decode()
+                }
+                return make_response(jsonify(responseObject)), 201
+            except Exception as e:
+                responseObject = {
+                    "status": "failure",
+                    "message": "An error occurred. Please try again"
+                }
+                return make_response(jsonify(responseObject)), 500
+        else:
+            responseObject = {
+                "status": "failure",
+                "message": "User already exists. Please log in."
+            }
+            return make_response(jsonify(responseObject)), 202
+
+# Define API resources
+
+registration_view = RegisterAPI.as_view('register_api')
+
+auth_blueprint.add_url_rule(
+    '/auth/register',
+    view_func=registration_view,
+    methods=['POST']
+)
--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -27,5 +27,29 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertEqual(response.status_code, 201)
 
 
+    def test_registered_with_already_registered_user(self):
+        """Tests that the user registration resource returns 202"""
+        user = User(
+            email='joe@gmail.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='test'
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(
+                data['message'] == "User already exists. Please log in."
+            )
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 202)
+
 if __name__ == "__main__":
     unittest.main()
